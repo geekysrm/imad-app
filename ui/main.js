1180db946cc9197cb9d732c111f738ca2eb0bc0b
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2017-08-15 20:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,24 @@
 //Counter code
 
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-  //MAke a req to the counter end point
+  //Create req obj
+  var request=new XMLHttpRequest();
   
   //capture respons e and store it in a variable
-  
+  reuest.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)  {
+        //Take some action
+        if(req.status===200){
+           var counter= request.responseText;
+           var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+        }
+    }
+  };
   //Render the variable in the correct span
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  //make req
+  request.open('GET','http://soumyacool2012.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
