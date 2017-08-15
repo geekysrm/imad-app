a0819cde4715dd2f2a2b9b22da1f10c69693cd6e
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2017-08-15 14:57:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //Change the txt of main-text 
 
 var element= document.getElementById('main-text');
-element.innerHTML='new value';
+element.innerHTML='OK';
 
 //moving image
 
