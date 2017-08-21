00ac435a3ce22254b0a4591b7452e93c22a34b11
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2017-08-22 01:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var config={
     host: 'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
-    
 };
 
 var app = express();
@@ -103,7 +102,7 @@ app.get('/', function (req, res) {
 
 
 var pool=new Pool(config);
-app.get('/test-db', function(res,req){
+app.get('/test-db', function(req,res){
   pool.query('SELECT * FROM test',function(err,result){
       if(err){
           res.status(500).send(err.toString());
