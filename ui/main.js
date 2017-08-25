0c4b76d66fd6dc26695edcc7f3d719b466986cce
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2017-08-25 20:53:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,29 @@
-//Counter code
-
-var button=document.getElementById('counter');
-
-button.onclick=function(){
-  //Create req obj
-  var request=new XMLHttpRequest();
-  
-  //capture respons e and store it in a variable
-  request.onreadystatechange=function(){
-    if(request.readyState===XMLHttpRequest.DONE)  {
-        //Take some action
-        if(request.status===200){
-           var counter= request.responseText;
-           var span=document.getElementById('count');
-             span.innerHTML=counter.toString();
+//Submit username and pswd to login
+var submit= document.getElementById('submit-btn');
+submit.onclick=function(){
+    var request= new XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+            alert('logged in successfully');
+        } else if (request.status===403){
+            alert('Username/Password is incorrect');
+        }
+        else if(request.status===500){
+            alert('Something went wrong on server');
         }
     }
-  };
-  //Render the variable in the correct span
-  //make req
-  request.open('GET','http://soumyacool2012.imad.hasura-app.io/counter',true);
-  request.send(null);
+};
+
+
+
+
+
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+  request.open('POST','http://soumyacool2012.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password: password}));
 };
