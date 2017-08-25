21c4c1afd3532a4f67f0e4da1154c3c28e2fbfa6
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2017-08-25 23:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,6 +141,11 @@ app.get('/check-login',function(req,res){
     }
 });
 
+app.get('/logout', function(req,res){
+    delete req.session.auth;
+    res.send('Logged Out');
+});
+
 var pool=new Pool(config);
 app.get('/test-db', function(req,res){
   pool.query('SELECT * FROM test',function(err,result){
